9c497059bcb6cb0451f5cffa4248dfc5a3008339
TheDragonSkull <sergibot6000@gmail.com>
2025-07-26 14:51:35 +0200

    Adding Advancements

--- src/main/java/net/thedragonskull/mobessencemod/datagen/loot/ModAdvancementLootTables.java
+++ src/main/java/net/thedragonskull/mobessencemod/datagen/loot/ModAdvancementLootTables.java
@@ -2,10 +2,14 @@ package net.thedragonskull.mobessencemod.datagen.loot;
 
 import net.minecraft.data.loot.LootTableSubProvider;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.InstrumentTags;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.functions.SetEnchantmentsFunction;
+import net.minecraft.world.level.storage.loot.functions.SetInstrumentFunction;
 import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
 import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 import net.thedragonskull.mobessencemod.MobEssenceMod;
@@ -35,6 +39,35 @@ public class ModAdvancementLootTables implements LootTableSubProvider {
                                         .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1))))
                         )
         );
+
+        consumer.accept(ResourceLocation.fromNamespaceAndPath(MobEssenceMod.MOD_ID, "advancements/neutral"),
+                LootTable.lootTable()
+                        .withPool(LootPool.lootPool()
+                                .setRolls(ConstantValue.exactly(1))
+                                .add(LootItem.lootTableItem(Items.CONDUIT)
+                                        .apply(SetItemCountFunction.setCount(ConstantValue.exactly(1))))
+                        )
+
+                        // (1 of 4 scream variants)
+                        .withPool(LootPool.lootPool()
+                                .setRolls(ConstantValue.exactly(1))
+                                .add(LootItem.lootTableItem(Items.GOAT_HORN)
+                                        .apply(SetInstrumentFunction.setInstrumentOptions(InstrumentTags.SCREAMING_GOAT_HORNS)))
+                        )
+        );
+
+        consumer.accept(ResourceLocation.fromNamespaceAndPath(MobEssenceMod.MOD_ID, "advancements/hostile"),
+                LootTable.lootTable()
+                        .withPool(LootPool.lootPool()
+                                .setRolls(ConstantValue.exactly(1))
+                                .add(LootItem.lootTableItem(Items.NETHERITE_SWORD)
+                                        .apply(new SetEnchantmentsFunction.Builder()
+                                                .withEnchantment(Enchantments.SHARPNESS, ConstantValue.exactly(5))
+                                        )
+                                )
+                        )
+        );
+
 
     }
 }
